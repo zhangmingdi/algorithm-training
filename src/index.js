586a927312606e6735ea3b26aa432ebd91a8452d
zhangmingdi <zhangmingdi@shein.com>
2021-06-27 23:19:38 +0800

    反转单项链表的中的部分链表

--- src/index.js
+++ src/index.js
@@ -1,7 +1,73 @@
 import list from './list'
-import str from './source'
 
 
+let last
 
 
-console.log('gggg', str)
+// 反转整个链表
+function reviseList(list) {
+
+    function doRevise(node) {
+        // 相当于进入这个点
+        if (node.next === null) {
+            last = node
+            return node
+        } else {
+            const preNode = doRevise(node.next)
+            preNode.next = node
+            node.next = null
+            return node
+        }
+
+    }
+    doRevise(list._head)
+}
+// reviseList(list)
+
+// 反转部分链表
+function reviseSomeList(header, i) {
+
+    let num = 0
+    let newHeader
+    let record
+
+    function doRevise(node) {
+        num++
+        // 相当于进入这个点
+        if (num === i) {
+            record = node.next
+            newHeader = node
+            return node
+        } else {
+            const preNode = doRevise(node.next)
+            preNode.next = node
+            node.next = null
+            return node
+        }
+
+    }
+    doRevise(header).next = record
+
+    return newHeader
+}
+// reviseSomeList(list, 3)
+// console.log('gggggggg', reviseSomeList(list._head, 4));
+
+// 反转区间链表
+function reviseIncludeList(header, from, to) {
+
+    if (from === 1) {
+        return reviseSomeList(header, to)
+    } else {
+        const newheader = reviseIncludeList(header.next, from - 1, to - 1)
+        header.next = newheader
+        return header
+    }
+
+
+}
+console.log('222', reviseIncludeList(list._head, 2, 4),);
+
+// reviseIncludeList(list)
+
+
