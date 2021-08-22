3be8091e5bc1d8ccd35b70810ec4b0e28eeee1de
zhangmingdi <zhangmingdi@shein.com>
2021-08-22 22:01:02 +0800

    链表3：合并链表

--- src/index.js
+++ src/index.js
@@ -1,49 +1,10 @@
 import list from './list'
 
-function sortArr(arr, start, end) {
-    if (start >= end) return
-    const rndom = Math.floor(Math.random() * (end - start + 1)) + start
-    const mid = arr[rndom]
-    let left = start
-    let right = end + 1
-
-    let i = start
-    //注意好闭区间，要想清楚，不然debugeer难，debugeer可以从小规模做起
-    while (i < right) {
-        if (arr[i].val < mid.val) {
-            [arr[left], arr[i]] = [arr[i], arr[left]]
-            left++
-            i++
-        } else if (arr[i].val > mid.val) {
-            right--
-            [arr[right], arr[i]] = [arr[i], arr[right]]
-        } else {
-            i++
-        }
-        // debugger
-    }
-    sortArr(arr, start, left - 1)
-    sortArr(arr, right, end)
-}
-
-// 排序链表 用一个数组 然后快速排序
-var reorderList2 = function (head) {
-    const arr = []
-
-    while (head !== null) {
-        arr.push(head)
-        head = head.next
-    }
-    sortArr(arr, 0, arr.length - 1)
-    for (let i = 0; i <= arr.length - 1; i++) {
-        arr[i].next = arr[i + 1] ? arr[i + 1] : null
-    }
-    console.log('最红输出结果', arr.map(item => item.val));
-};
-// reorderList(list.head)
-
-// 有序链表合并
-function merge(left, right) {
+//双指针，合并排序，迭代法
+function mergeTwoLists0(left, right) {
+    if (left === null) return right
+    if (right === null) return left
+    if (left === null && right === null) return null
     // 合并和数组归并当合并一样
     let pointer
     // 保存头部
@@ -59,38 +20,30 @@ function merge(left, right) {
         pointer = node
         left.val > right.val ? right = right.next : left = left.next
     }
+    pointer.next = left ? left : right
 
-    while (left !== null) {
-        pointer.next = left
-        pointer = left
-        left = left.next
-    }
-    while (right !== null) {
-        pointer.next = right
-        pointer = right
-        right = right.next
-    }
+    // while (left !== null) {
+    //     pointer.next = left
+    //     pointer = left
+    //     left = left.next
+    // }
+    // while (right !== null) {
+    //     pointer.next = right
+    //     pointer = right
+    //     right = right.next
+    // }
     return header
 }
 
-// 链表排序 归并方法 空间O(1)时间O(nlog(n)) 使用归并排序
-//!!另外归并还有一种自底向上
-var sortList = function (head) {
-    // 注意当链表只有一个的时候或者没有的时候就不要排序
-    if (head === null || head.next === null) return head
-    let l = head
-    let r = head
-    while (r !== null && r.next !== null) {
-        r = r.next.next
-        //这样做当链表个数为双个数，可让慢指针指向中间当前一个
-        if (r !== null) {
-            l = l.next
-        }
+function mergeTwoLists(left, right) {
+    if (left === null || right === null) {
+        return left === null ? right : left
     }
-    const right = l.next
-    l.next = null
-    const leftHeader = sortList(head)
-    const rihghtHeader = sortList(right)
-    return merge(leftHeader, rihghtHeader)
-};
-// console.log('sss', sortList(list.head));
+    else if (left.val > right.val) {
+        right.next = mergeTwoLists(left, right.next)
+        return right
+    } else {
+        left.next = mergeTwoLists(left.next, right)
+        return left
+    }
+}
