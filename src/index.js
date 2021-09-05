dbc06533f70e76865c8b84f27d20a489352f9d86
zhangmingdi <zhangmingdi@shein.com>
2021-09-05 17:54:04 +0800

    二叉搜索树验证

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 import list from './list'
-
-// 二叉树先序遍历 非递归版
-var preorderTraversal0 = function (root) {
-    var list = []
-    var stack = []
-    while (root != null || stack.length > 0) {
-        while (root !== null) {
-            list.push(root.val)
-            stack.push(root)
-            root = root.left
-        }
-        //弹出来之后直接判断右边，然后继续压栈
-        root = stack.pop()
-        root = root.right
-    }
-    return list
-};
-
 function TreeNode(val, left, right) {
     this.val = (val === undefined ? 0 : val)
     this.left = (left === undefined ? null : left)
@@ -29,118 +11,35 @@ const root = new TreeNode(
     )
 )
 
-// 二叉树先序遍历 左神版 非常容易理解
-var preorderTraversal1 = function (root) {
-    if (root === null) return root
-    const stack = []
-    const list = []
-    stack.push(root)
-    // 每次都弹出一个都先压右再压左
-    while (stack.length > 0) {
+// 验证二叉搜索树 （递归） 中序遍历是顺序即可
+var isValidBST0 = function (root) {
+    //前一个值无限小
+    let prevalue = -Infinity
 
-        const node = stack.pop()
-        list.push(node.val)
-        if (node.right !== null) {
-            stack.push(node.right)
-        }
-        if (node.left !== null) {
-            stack.push(node.left)
-        }
+    function doIsBST(root) {
+        if (root === null) return true
+        const isBST = doIsBST(root.left)
+        if (prevalue >= root.val || !isBST) return false
+        prevalue = root.val
+        return doIsBST(root.right)
+
     }
-    return list
+    return doIsBST(root)
 };
-// console.log('ssss', preorderTraversal(root));
-
-// 二叉树的后续遍历 自己的做法 
-var postorderTraversal2 = function (root) {
-    const list = []
-    const stack = []
-
-    while (stack.length > 0 || root !== null) {
-
-        while (root !== null) {
-            //通过记录num来判断是第几次访问
-            root.num = 1
-            stack.push(root)
-            root = root.left
-        }
-        // 回溯的时候不要弹出来
-        root = stack[stack.length - 1]
-        if (root.num === 2) {
-            const node = stack.pop()
-            node.num = undefined
-            list.push(node.val)
-            root = null
-        } else {
-            // 记下来第二次访问，下次可以直接弹出来
-            root.num = 2
-            root = root.right
-        }
-    }
-    return list
-};
-
-
-// 二叉树的后续遍历 双栈
-var postorderTraversal3 = function (root) {
-    const stack = []
-    const backStack = []
-    const list = []
-    if (root === null) return list
-    stack.push(root)
-    while (stack.length > 0) {
-        const node = stack.pop()
-        backStack.push(node)
-        //注意哦是先压左再压右哦
-        if (node.left !== null) {
-            stack.push(node.left)
-        }
-        if (node.right !== null) {
-            stack.push(node.right)
-        }
-
-    }
-    while (backStack.length > 0) {
-        list.push(backStack.pop().val)
-    }
-    return list
-};
-
-// 二叉树中序遍历 非递归版
-var preorderTraversal0 = function (root) {
-    var list = []
-    var stack = []
-    while (root != null || stack.length > 0) {
+// 验证二叉搜索树 （遍历）就是中序遍历是顺序即可
+var isValidBST = function (root) {
+    let stack = []
+    let prevalue = -Infinity
+    while (stack.length !== 0 || root !== null) {
         while (root !== null) {
             stack.push(root)
             root = root.left
         }
-        // 第一次弹出来就是中序遍历
         root = stack.pop()
-        list.push(root.val)
+        if (root.val <= prevalue) return false
+        prevalue = root.val
         root = root.right
     }
-    return list
-};
 
-// 中序遍历 左神
-var inorderTraversal = function (root) {
-    const list = []
-    const stack = []
-
-    while (root !== null || stack.length > 0) {
-        // 有左一直压左
-        if (root !== null) {
-            stack.push(root)
-            root = root.left
-        } else {
-            // 到尽头就可以弹出 ，继续压弹出来的右
-            root = stack.pop()
-            list.push(root.val)
-            root = root.right
-        }
-    }
-
-    return list
-
+    return true
 };
