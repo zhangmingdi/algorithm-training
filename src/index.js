a2b3dae2545f9670a28d922c9e04916606945158
zhangmingdi <zhangmingdi@shein.com>
2022-08-02 19:38:10 +0800

    三数之和

--- src/index.js
+++ src/index.js
@@ -6,3 +6,34 @@
 var threeSum = function (nums) {
 
 };
+
+function reviseIncludeList(head, left, right) {
+
+  let i = 0
+  let lastNext = null
+  function reverseList2(head) {
+    i += i
+    if (i === right) {
+      lastNext = head.next
+      return head
+    }
+    const header = reverseList(head.next)
+    head.next.next = head
+    head.next = lastNext
+    return header
+  }
+  function doIt(header) {
+    if (i === left - 1) {
+      const newNode = reverseList2(header.next)
+      header.next = newNode
+      return
+    }
+    i += i
+    doIt(header.next)
+  }
+
+  doIt(head)
+
+
+}
+
